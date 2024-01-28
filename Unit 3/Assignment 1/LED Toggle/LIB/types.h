64010df8598902f410c992549048851468cade16
DevYehia <yehiayes20@gmail.com>
2024-01-28 15:02:34 +0200

    Did Assignment 1 Unit 3

---
+++ Unit 3/Assignment 1/LED Toggle/LIB/types.h
@@ -0,0 +1,26 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char uint8;
+typedef signed char sint8;
+
+typedef unsigned short uint16;
+typedef signed short sint16;
+
+typedef unsigned int uint32;
+typedef signed int sint32;
+
+typedef volatile uint8 v_uint8;
+typedef volatile sint8 v_sint8;
+
+typedef volatile uint16 v_uint16;
+typedef volatile sint16 v_sint16;
+
+typedef volatile uint32 v_uint32;
+typedef volatile sint32 v_sint32;
+
+
+
+
+
+#endif
