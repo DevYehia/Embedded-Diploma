ddb656783192d36c7593f3539893eb0979b64e17
DevYehia <yehiayes20@gmail.com>
2024-02-07 04:53:40 +0200

    Fixed a mistake where Assignment 1's code was just copied from the lecture, instead i wrote my own code again

--- Unit 3/Assignment 1 (LED Toggle)/LED Toggle/Src/main.c
+++ Unit 3/Assignment 1 (LED Toggle)/LED Toggle/Src/main.c
@@ -1,48 +1,48 @@
 /*
  * main.c
  *
- *  Created on: Nov 21, 2023
- *      Author: Dell
+ *  Created on: Feb 7, 2024
+ *      Author: Yehia
  */
 
 
-//#include "type_def.h"
-#include <stdint.h>
-typedef volatile unsigned int vuint32_t;
+#include "../LIB/bitwise.h"
+#include "../LIB/types.h"
+
 #define RCC_BASE 0x40021000
 #define GPIOA_BASE 0x40010800
-#define RCC_ABP2ENR *((volatile uint32_t *)(RCC_BASE + 0x18))
-#define GPIOA_CRH *((volatile uint32_t *) (GPIOA_BASE + 0x04))
-#define GPIOA_ODR *((volatile uint32_t *) (GPIOA_BASE + 0x0c))
+
+#define RCC_ABP2ENR *((v_uint32 *)(RCC_BASE + 0x18))
+#define GPIOA_CRH *((v_uint32 *) (GPIOA_BASE + 0x04))
+#define GPIOA_ODR *((v_uint32*) (GPIOA_BASE + 0x0c))
 
 /* bit fields*/
 
-#define RCC_IOPAEN (1 << 2)
-#define GPIOA13 (1UL << 13)
+#define RCC_IOPAEN 2
+#define GPIOA13 13
 
 
 typedef union {
-    vuint32_t ALL_fields;
+    v_uint32 ALL_fields;
     struct{
-        vuint32_t reserved :13;
-        vuint32_t P_13:1;
+        v_uint32 reserved :13;
+        v_uint32 P_13:1;
     } Pin;
 
 } R_ODR_t;
 
-volatile R_ODR_t * R_ODR = (volatile R_ODR_t*) (GPIOA_BASE + 0x0C);
 
 
 int main(void){
-    RCC_ABP2ENR |= RCC_IOPAEN;
+    SET_BIT(RCC_ABP2ENR,RCC_IOPAEN);
     GPIOA_CRH &= 0xFF0FFFFF;
     GPIOA_CRH |= 0x00200000;
-
+    CLR_BIT(GPIOA_ODR,GPIOA13);
     while(1){
-        R_ODR->Pin.P_13 = 1;
+        TOGGLE_BIT(GPIOA_ODR,GPIOA13);
         int i;
         for(i = 0; i < 500000; i++);
-        R_ODR->Pin.P_13 = 0;
+        TOGGLE_BIT(GPIOA_ODR,GPIOA13);
         for(i = 0; i < 500000; i++);
     }
 }
