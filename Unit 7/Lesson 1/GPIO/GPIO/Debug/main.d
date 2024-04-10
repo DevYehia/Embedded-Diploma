2eb7febbf319a42a7d766bd13994f34d7cfa5fbb
DevYehia <yehiayes20@gmail.com>
2024-04-11 00:53:06 +0200

    Organized Folders of Unit 6

---
+++ Unit 7/Lesson 1/GPIO/GPIO/Debug/main.d
@@ -0,0 +1,10 @@
+main.d main.o: .././main.c \
+ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\stdio.h \
+ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\features.h \
+ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\bits\alltypes.h
+
+c:\program\ files\microchip\xc8\v2.36\avr\avr\include\stdio.h:
+
+c:\program\ files\microchip\xc8\v2.36\avr\avr\include\features.h:
+
+c:\program\ files\microchip\xc8\v2.36\avr\avr\include\bits\alltypes.h:
---
+++ Unit 7/Lesson 1/GPIO/GPIO/main.c
@@ -0,0 +1,52 @@
+/*
+ * main.c
+ *
+ * Created: 4/10/2024 11:59:56 PM
+ *  Author: user
+ */ 
+#include <stdio.h>
+//bitwise Macros
+#define SET_BIT(reg,bit) (reg |= (1<<bit))
+#define CLR_BIT(reg,bit) (reg &= ~(1<<bit))
+#define TGL_BIT(reg,bit) (reg ^= (1<<bit))
+
+
+#define DDRD   *(volatile unsigned char *)0x31
+#define PORTD  *(volatile unsigned char *)0x32
+
+int x;
+
+void setDirection(void){
+	SET_BIT(DDRD,4);
+	SET_BIT(DDRD,5);
+	SET_BIT(DDRD,6);
+	SET_BIT(DDRD,7);
+}
+
+void delay(void){
+	for(unsigned int i = 0 ; i < 60000 ; i++); //Note: int size is 2 bytes
+}
+int main(void)
+{
+	x = sizeof(long);
+	setDirection();
+    while(1)
+    {
+		SET_BIT(PORTD,5);
+		delay();
+		CLR_BIT(PORTD,5);
+
+		SET_BIT(PORTD,6);
+		delay();
+		CLR_BIT(PORTD,6);
+		
+		SET_BIT(PORTD,7);
+		delay();
+		CLR_BIT(PORTD,7);
+				
+		SET_BIT(PORTD,4);
+		delay();
+		CLR_BIT(PORTD,4);		
+
+    }
+}
