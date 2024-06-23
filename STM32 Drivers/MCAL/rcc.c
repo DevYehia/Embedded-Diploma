e0dab41cf65bee32ed552a21fbe8ffb3fe2547fd
DevYehia <yehiayes20@gmail.com>
2024-06-23 23:41:09 +0300

    Modified rcc driver to be debug friendly

--- STM32 Drivers/MCAL/rcc.c
+++ STM32 Drivers/MCAL/rcc.c
@@ -1,13 +1,15 @@
 #include "rcc.h"
 
 
-#define APBconvertPreScaleBitsToShift(bits) ((bits) < 4 : 0 ? (bits) - 3)
+#define APBconvertPreScaleBitsToShift(bits) ((bits) < 4 ? 0 : (bits) - 3)
 
 
 const uint16_t AHBPreScales[16] = {1, 1, 1, 1, 1, 1, 1, 1, 2, 4, 8, 16, 64, 128, 256, 512};
 
 uint32_t RCC_Get_SYSCLK_freq(void){
-	if(((RCC->CFGR) >> 2) & 0x3 == 0) return HSI_CLK_FREQ;
+	if((((RCC->CFGR) >> 2) & 0x3) == 0){ 
+        return HSI_CLK_FREQ;
+    }
 
 }
 
